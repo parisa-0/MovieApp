a31fd9991f400ec6c65f476cc5f7f897aeadeaaa
Parisa <134794618+parisa-0@users.noreply.github.com>
2024-03-17 19:59:05 +0000

    Update UserRepository.java

--- Movies/src/main/java/com/example/Movies/UserRepository.java
+++ Movies/src/main/java/com/example/Movies/UserRepository.java
@@ -5,5 +5,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+     /*
+    @Query("SELECT u FROM User u where u.username like ?1%")
+    List<User> findName(String username);
+     */
+    @Query("SELECT u FROM User u where u.username = ?1 and u.password = ?2")
+    List<User> findName(String username, String password);
 
 }
